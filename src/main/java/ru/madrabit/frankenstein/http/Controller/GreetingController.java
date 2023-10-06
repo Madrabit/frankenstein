78a08f8a2dd3e5cf08ecae2f735555c9be2c8bc1
Madrabit <madrabot@gmail.com>
2023-10-06 22:09:10 +0300

    thymleaf starter

--- src/main/java/ru/madrabit/frankenstein/http/Controller/GreetingController.java
+++ src/main/java/ru/madrabit/frankenstein/http/Controller/GreetingController.java
@@ -8,10 +8,11 @@ import ru.madrabit.frankenstein.dto.UserReadDTO;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Controller
 @RequestMapping("/api/v1")
-@SessionAttributes("userReadDTO")
+@SessionAttributes("user")
 public class GreetingController {
 
 
@@ -19,13 +20,15 @@ public class GreetingController {
 //    public UserReadDTO createUserReadDTO(@RequestParam("id") Long id, @RequestParam("username") String username) {
 //        return new UserReadDTO(id, username);
 //    }
-    @GetMapping("/hello")
-    public String hello(Model model,
-                        HttpServletRequest request,
-                        @ModelAttribute UserReadDTO userReadDTO) {
-//        model.addAttribute("user", new UserReadDTO(1L, "Ivan"));
-        return "greeting/hello";
-    }
+@GetMapping("/hello")
+public String hello(Model model,
+                    HttpServletRequest request,
+                    @ModelAttribute("userReadDto") UserReadDTO userReadDto) {
+
+    model.addAttribute("user", userReadDto);
+
+    return "greeting/hello";
+}
 
     @GetMapping("/hello/{id}")
     public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
@@ -40,8 +43,8 @@ public class GreetingController {
         return modelAndView;
     }
 
-    @GetMapping("/buy")
-    public String buy(Model model, @SessionAttribute("userReadDTO") UserReadDTO userReadDTO) {
-        return "greeting/buy";
+    @GetMapping("/bye")
+    public String bye(@SessionAttribute("user") UserReadDTO user, Model model) {
+        return "greeting/bye";
     }
 }
